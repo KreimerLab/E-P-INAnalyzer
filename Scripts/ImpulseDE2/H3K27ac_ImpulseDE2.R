db1939e9c5f75b795c7c2690deeb3b94f7b571fd
KreimerLab <144374555+KreimerLab@users.noreply.github.com>
2023-11-17 07:22:18 -0500

    Delete Scripts directory

--- Scripts/ImpulseDE2/H3K27ac_ImpulseDE2.R
+++
@@ -1,9 +0,0 @@
-# Load Libraries
-library(ImpulseDE2)
-source('/home/wbd20/Kreimer_Lab/Scripts/ImpulseDE2/ImpulseDE2.R')
-
-# ImpulseDE2 Analysis
-ImpulseDE2_H3K27ac(comparisons = "/home/wbd20/Kreimer_Lab/H3K27ac/ImpulseDE2/Comparisons/0hrs_3hrs_6hrs_12hrs_24hrs_48hrs_72hrs_H3K27ac.ImpulseDE2_comparisons.csv",
-                   conditions = "/home/wbd20/Kreimer_Lab/H3K27ac/ImpulseDE2/Conditions/0hrs_3hrs_6hrs_12hrs_24hrs_48hrs_72hrs_H3K27ac.ImpulseDE2_conditions.csv",
-                   results = "/home/wbd20/Kreimer_Lab/H3K27ac/ImpulseDE2/Results/H3K27ac.ImpulseDE2_results.csv",
-                   padj_threshold = 0.01)
--- Scripts/ImpulseDE2/RNASeq_ImpulseDE2.R
+++
@@ -1,9 +0,0 @@
-# Load Libraries
-library(ImpulseDE2)
-source('/home/wbd20/Kreimer_Lab/Scripts/ImpulseDE2/ImpulseDE2.R')
-
-# ImpulseDE2 Analysis
-ImpulseDE2_RNASeq(comparisons = "/home/wbd20/Kreimer_Lab/RNASeq/ImpulseDE2/Comparisons/0hrs_3hrs_6hrs_12hrs_24hrs_48hrs_72hrs_RNASeq.ImpulseDE2_comparisons.csv",
-                  conditions = "/home/wbd20/Kreimer_Lab/RNASeq/ImpulseDE2/Conditions/0hrs_3hrs_6hrs_12hrs_24hrs_48hrs_72hrs_RNASeq.ImpulseDE2_conditions.csv",
-                  results = "/home/wbd20/Kreimer_Lab/RNASeq/ImpulseDE2/Results/RNASeq.ImpulseDE2_results.csv",
-                  padj_threshold = 0.01)

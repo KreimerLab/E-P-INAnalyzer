6bd544b73849f8743aaa646ba5debaed52732451
Will DeGroat <94496627+WillDeGroat@users.noreply.github.com>
2023-11-17 09:25:57 -0500

    Delete Scripts directory

--- Scripts/ImpulseDE2/RNASeq_ImpulseDE2.R
+++
@@ -1,9 +0,0 @@
-# Load Libraries
-library(ImpulseDE2)
-source('/E-P-INs/Scripts/ImpulseDE2/ImpulseDE2.R')
-
-# ImpulseDE2 Analysis
-ImpulseDE2_RNASeq(comparisons = "/E-P-INs/RNASeq/ImpulseDE2/Comparisons/0hrs_3hrs_6hrs_12hrs_24hrs_48hrs_72hrs_RNASeq.ImpulseDE2_comparisons.csv",
-                  conditions = "/E-P-INs/RNASeq/ImpulseDE2/Conditions/0hrs_3hrs_6hrs_12hrs_24hrs_48hrs_72hrs_RNASeq.ImpulseDE2_conditions.csv",
-                  results = "/E-P-INs/RNASeq/ImpulseDE2/Results/RNASeq.ImpulseDE2_results.csv",
-                  padj_threshold = 0.01)
